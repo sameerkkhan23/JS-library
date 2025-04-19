3c31a556671232e41e95630cbbc994e45ff06156
sameerkkhan23 <sameerkkhan2351@gmail.com>
2025-04-19 19:30:32 +0000

    02_Datatypes

---
+++ .devcontainer/01_basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS Codes as newer version
+
+// alert( 3 + 3 )  // we are using node.js , not browser
+
+console.log("sameer");
+
+let name = "Sameer  khan"
+let age = 18
+let isLoggeedIn = false
+let state;
+
+
+// number ==> 2 to power 53
+// bigInt
+// String => ""
+// boolean => true / false
+// null =>  standalone value
+// undefined => value not be assigned
+// symbol =>  unique
+
+console.log(typeof "Sameer khan"); 
+
+// typeof   // typeof is a function to kmow about all datatypes
+
