fd3b3c5f26d0f6e5cf03baf1eeacd5ae1d7eff47
sameerkkhan23 <sameerkkhan2351@gmail.com>
2025-04-19 19:30:50 +0000

    03_conversionOperation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "sameer"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33"  =>  33
+// "33abs"  => NaN
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+
+// true => 1;  false => 0 
+// ""  => false 
+// "Sameer"  => true
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
