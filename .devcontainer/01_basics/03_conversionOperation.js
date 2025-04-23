adb82c30e79c330ae4b83d05c4ac9d72a341abb8
sameerkkhan23 <sameerkkhan2351@gmail.com>
2025-04-23 08:07:29 +0000

    03_conversion

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -28,5 +28,37 @@ let someNumber = 55
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// *********************************** Operations ************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+*/
+
+let str1 = "Hello"
+let str2 = " Sameer khan"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log( 1 + "2");
+console.log( "1" + 2);
+console.log( 1 + 2 + 2);
+console.log( "1" + 2 + 2);
+console.log( 1 + 2 + "2");
+
+console.log(( 3 + 6) * 3 % 2 );
+
